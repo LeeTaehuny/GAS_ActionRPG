545983692b0d65be92bb8269f95fc46547d5d7b5
LeeTaes <apth1023@naver.com>
2025-06-28 08:56:06 +0900

    Define GAS Input Tag
    GAS에서 사용하기 위한 입력 태그 추가

---
+++ Source/GAS_ActionRPG/Private/ARGameplayTags.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ARGameplayTags.h"
+
+namespace ARGameplayTags
+{
+	/** Input Tags **/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look")
+}
---
+++ Source/GAS_ActionRPG/Public/ARGameplayTags.h
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace ARGameplayTags
+{
+	/** Input Tags **/
+	GAS_ACTIONRPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	GAS_ACTIONRPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
