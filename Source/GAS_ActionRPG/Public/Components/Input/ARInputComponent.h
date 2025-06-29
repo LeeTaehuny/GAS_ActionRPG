a76768e13f7696e1dde14e949c0a42e046f7ed3a
LeeTaeHyun <apth1023@naver.com>
2025-06-30 08:35:06 +0900

    Custom Input Component
    Default 입력 컴포넌트로 사용하기 위한 Input Component 추가

---
+++ Source/GAS_ActionRPG/Public/Components/Input/ARInputComponent.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+#include "ARInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_ACTIONRPG_API UARInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+public:
+	template<class UserObject, typename CallbackFunc>
+	void BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func);
+};
+
+template<class UserObject, typename CallbackFunc>
+inline void UARInputComponent::BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func)
+{
+	checkf(InInputConfig, TEXT("Input Config Data Asset is Null, Can not proceed with binding"));
+
+	if (UInputAction* FoundAction = InInputConfig->FindNativeInputActionByTag(InInputTag))
+	{
+		BindAction(FoundAction, TriggerEvent, ContextObject, Func);
+	}
+}
