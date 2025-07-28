e62097e5616cfb17b7d4cf7f25e6e754483dd70c
LeeTaeHyun <apth1023@naver.com>
2025-07-29 08:39:01 +0900

    BlueprintLibrary Func Add
    특정 GameplayTag를 액터에 부여할 수 있는 함수 구현

---
+++ Source/GAS_ActionRPG/Private/ARFunctionLibrary.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ARFunctionLibrary.h"
+#include "AbilitySystem/ARAbilitySystemComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+
+UARAbilitySystemComponent* UARFunctionLibrary::NativeGetASCFromActor(AActor* InActor)
+{
+	check(InActor);
+
+	return CastChecked<UARAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(InActor));
+}
+
+void UARFunctionLibrary::AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag TagToAdd)
+{
+	UARAbilitySystemComponent* ASC = NativeGetASCFromActor(InActor);
+
+	// 만약 해당 컴포넌트에 TagToAdd 태그가 존재하지 않는 경우 태그를 추가합니다.
+	if (!ASC->HasMatchingGameplayTag(TagToAdd))
+	{
+		ASC->AddLooseGameplayTag(TagToAdd);
+	}
+}
+
+void UARFunctionLibrary::RemoveGameplayTagFromActorIfFound(AActor* InActor, FGameplayTag TagToRemove)
+{
+	UARAbilitySystemComponent* ASC = NativeGetASCFromActor(InActor);
+
+	// 만약 해당 컴포넌트에 TagToAdd 태그가 존재하는 경우 태그를 제거합니다.
+	if (ASC->HasMatchingGameplayTag(TagToRemove))
+	{
+		ASC->RemoveLooseGameplayTag(TagToRemove);
+	}
+}
+
+bool UARFunctionLibrary::NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck)
+{
+	UARAbilitySystemComponent* ASC = NativeGetASCFromActor(InActor);
+
+	return ASC->HasMatchingGameplayTag(TagToCheck);
+}
+
+void UARFunctionLibrary::BP_DoseActorHaveTag(AActor* InActor, FGameplayTag TagToCheck, EActionRPGConfirmType& OutConfirmType)
+{
+	OutConfirmType = NativeDoesActorHaveTag(InActor, TagToCheck) ? EActionRPGConfirmType::Yes : EActionRPGConfirmType::No;
+}
---
+++ Source/GAS_ActionRPG/Public/ARFunctionLibrary.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ARFunctionLibrary.generated.h"
+
+UENUM()
+enum class EActionRPGConfirmType : uint8
+{
+	Yes,
+	No,
+};
+
+class UARAbilitySystemComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_ACTIONRPG_API UARFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	// C++
+	static UARAbilitySystemComponent* NativeGetASCFromActor(AActor* InActor);
+
+	// C++ & BP
+	UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary")
+	static void AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag TagToAdd);
+
+	UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary")
+	static void RemoveGameplayTagFromActorIfFound(AActor* InActor, FGameplayTag TagToRemove);
+
+	// C++
+	static bool NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck);
+
+	// BP
+	UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary", meta = (DisplayName = "Does Actor Have Tag", ExpandEnumAsExecs = "OutConfirmType"))
+	static void BP_DoseActorHaveTag(AActor* InActor, FGameplayTag TagToCheck, EActionRPGConfirmType& OutConfirmType);
+};
